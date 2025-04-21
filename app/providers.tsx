c6a6f7668e955e3212ad7917274cd9a5c4e8eb35
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2025-04-22 01:56:22 +0700

    feat: setup graz

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,6 +3,9 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { SessionProvider } from "next-auth/react";
+import { GrazProvider } from "graz";
+import { cosmoshub } from "graz/chains";
 
 // Create a client
 const queryClient = new QueryClient();
@@ -11,9 +14,17 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   const [client] = React.useState(queryClient);
 
   return (
-    <QueryClientProvider client={client}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <SessionProvider>
+      <QueryClientProvider client={client}>
+        <GrazProvider
+          grazOptions={{
+            chains: [cosmoshub],
+          }}
+        >
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </GrazProvider>
+      </QueryClientProvider>
+    </SessionProvider>
   );
 }
