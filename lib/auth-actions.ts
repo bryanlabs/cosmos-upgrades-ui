5f8d6fc824b4b6239c82d569645e3ded7eb73d92
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2025-04-22 22:48:13 +0700

    feat: add auth actions

---
+++ lib/auth-actions.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import { signIn, signOut } from "./auth";
+
+export const login = async (provider: string) => {
+  await signIn(provider, {
+    redirectTo: "/",
+  });
+};
+
+export const logout = async () => {
+  await signOut({
+    redirectTo: "/",
+  });
+};
