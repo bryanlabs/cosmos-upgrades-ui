4c594d3f26c970f0efb6bd618d5ca546782d883b
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2025-04-23 02:11:09 +0700

    feat: add icons

---
+++ components/icons/index.tsx
@@ -0,0 +1,6 @@
+import KeplrIcon from "./keplr";
+import LeapIcon from "./leap";
+import MetamaskIcon from "./metamask";
+import WalletConnect from "./wallet-connect";
+
+export { KeplrIcon, LeapIcon, MetamaskIcon, WalletConnect };
