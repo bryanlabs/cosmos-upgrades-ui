5d50d88f9a096cb0b6345edf20004e1b895c1bd1
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2025-04-25 20:54:43 +0700

    feat: add discord and slack icons

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -2,5 +2,14 @@ import KeplrIcon from "./keplr";
 import LeapIcon from "./leap";
 import MetamaskIcon from "./metamask";
 import WalletConnect from "./wallet-connect";
+import SlackIcon from "./slack";
+import DiscordIcon from "./discord";
 
-export { KeplrIcon, LeapIcon, MetamaskIcon, WalletConnect };
+export {
+  KeplrIcon,
+  LeapIcon,
+  MetamaskIcon,
+  WalletConnect,
+  SlackIcon,
+  DiscordIcon,
+};
