5c04f704751ef2f358beb7fdf54eb3fd28249013
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2025-05-08 09:28:23 +0700

    feat: update signin button

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-"use client";
-
 import { ChainSection } from "@/components/sections/chain-section";
-import { useUserData } from "@/hooks/useUserData";
 
 export default function Home() {
-  useUserData();
   return (
     <div className="container mx-auto px-4 py-8">
       <ChainSection />
